6a56eddce98e7b1634a545d314be9043b6db73c2
tangyue1004@gmail.com <tangyue1004@gmail.com@7c0b1484-3751-40fc-9704-58265e63bfdb>
2012-07-27 01:20:37 +0000

    main functions of adding student
    git-svn-id: https://svn.sinaapp.com/pktangyue@10 7c0b1484-3751-40fc-9704-58265e63bfdb

--- 1/application/controllers/student.php
+++ 1/application/controllers/student.php
@@ -1,9 +1,28 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Student extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('student_model');
+        $this->load->model('region_model');
+    }
     public function index() {
+        $provinces = $this->region_model->get_provinces();
         $this->parser->parse('/student.html', array(
-            'title' => 'student manage'
+            'title' => 'student manage',
+            'provinces' => $provinces
+        ));
+    }
+    public function add() {
+        if (!$this->input->is_ajax_request()) {
+            return;
+        }
+        $data = json_decode(file_get_contents('php://input'));
+        $this->student_model->insert($data);
+    }
+    public function get_cities($province_id) {
+        echo json_encode(array(
+            'cities' => $this->region_model->get_cities_by_id($province_id)
         ));
     }
 }
